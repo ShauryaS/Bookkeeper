37448038a30abf45a9e566340736b5dde976f0d9
Shaurya Srivastava <shauryas481@gmail.com>
2016-07-16 18:20:32 -0700

    Began Backend --- Login in progress

---
+++ Bookkeeping/Params.swift
@@ -0,0 +1,11 @@
+//
+//  Params.swift
+//  Bookkeeping
+//
+//  Created by Shaurya Srivastava on 7/16/16.
+//  Copyright © 2016 Shaurya Srivastava. All rights reserved.
+//
+
+import Foundation
+
+let url = NSURL(string: "https://cfo-online.com/")
